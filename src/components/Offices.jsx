e23f18b68d2f497c14288701ceb261d253aac7ff
Hewlbern <michael.t.holborn@gmail.com>
2023-07-15 21:10:18 +1000

    first singular

---
+++ src/components/Offices.jsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx'
+
+function Office({ name, children, invert = false }) {
+  return (
+    <address
+      className={clsx(
+        'text-sm not-italic',
+        invert ? 'text-neutral-300' : 'text-neutral-600'
+      )}
+    >
+      <strong className={invert ? 'text-white' : 'text-neutral-950'}>
+        {name}
+      </strong>
+      <br />
+      {children}
+    </address>
+  )
+}
+
+export function Offices({ invert = false, ...props }) {
+  return (
+    <ul role="list" {...props}>
+      <li>
+        <Office name="Copenhagen" invert={invert}>
+          Stone and Chalk
+          <br />
+Sydney Central        </Office>
+      </li>
+      
+    </ul>
+  )
+}
