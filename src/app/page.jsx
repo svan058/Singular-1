5b8d74d4ec09cc259e2959c1c749c6e68cf753ca
Stefan van der Waal <stefan.vanderwaal@iag.com.au>
2023-08-03 16:18:06 +1000

    Changed title

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -177,12 +177,11 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award winning developer studio based in Sydney.
+            Data analytics and Digital advisory and software development consultancy
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            We are developer studio working at the intersection of design and
-            technology. It’s a really busy intersection though — a lot of our
-            staff have been involved in hit and runs.
+            We are developer studio working at the intersection of data and digital
+            technology.
           </p>
         </FadeIn>
       </Container>
