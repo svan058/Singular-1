f302557f558c34bcab4465440a07288868b7dfa0
Stefan van der Wel <stefan.vanderwel@gmail.com>
2023-09-03 14:33:22 +1000

    jjjjjjj#

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -37,7 +37,7 @@ function Clients() {
       <Container>
         <FadeIn className="flex items-center gap-x-8">
           <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
-            We’ve worked with hundreds of amazing people
+            We’ve worked with two of amazing people
           </h2>
           <div className="h-px flex-auto bg-neutral-800" />
         </FadeIn>
