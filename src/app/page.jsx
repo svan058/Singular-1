8e58b930239cf7df78b78246b857d2cb6337be8e
Hewlbern <michael.t.holborn@gmail.com>
2023-10-31 16:44:30 +1100

    work stuff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -162,10 +162,10 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Data & Analytics advisory in Australia & New Zealand
+            Digital and Data Consulting Services
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            At Singular, we’re not just navigating the future; we’re creating it. 
+           Pareto’s principle is that 20% of the change can drive 80% of the outcome. Singular specialises in  we’re not just navigating the future; we’re creating it. 
             We specialize in the kind of disruptive strategy and technical architecture that doesn’t just improve your business, but reinvents it—turning data into something more than information, into vision.
            Forget the next quarter; we’re building the roadmap for your next quarter-century.
           </p>
